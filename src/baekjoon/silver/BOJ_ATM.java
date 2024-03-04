e87465f828bf348b8dff6f8b6ede67512c022cc3
GBGreenBravo <gbgreenbravo@gmail.com>
2024-03-04 09:52:01 +0900

    feat: BOJ_ATM

---
+++ src/baekjoon/silver/BOJ_ATM.java
@@ -0,0 +1,32 @@
+package baekjoon.silver;
+
+/*
+04:54
+
+- 그리디
+ */
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class BOJ_ATM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o));
+
+        for (int i = 0; i < n; i++) {
+            queue.add(sc.nextInt());
+        }
+
+        int answer = 0;
+        int count = n;
+        for (int i = 0; i < n; i++) {
+            answer += queue.poll() * count--;
+        }
+
+        System.out.println(answer);
+    }
+}
